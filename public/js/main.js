e7e4dec114af9eca086773cb1ac8032a90900f0b
ttrojan <ttrojan@ttrojan.local>
2012-12-26 02:08:19 -0800

    Still working on it.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,23 @@
-$('.peppable').pep({ 
+$('.peppable').pep({
+	//debug: true,
 	constrainToParent: true,
-	drag: function(ev,obj){ 
+	drag: function(ev,obj){
 		var el = $(obj.el);
 		var val = (el.parent().height() - el.position().top) / el.parent().height();
-		console.log( el.position().left, el.position().top, val )
+		switch( ev.type ){
+			case "mousemove":
+			console.log( "Left:", el.position().left, "Top:", el.position().top, val + "%" );
+			break;
+		}
 	},
-	stop: function(ev,obj){ 
+	stop: function(ev,obj){
 		//$(obj.el).css({ "-webkit-transform": "scale("+ 0.3 +")" });
 	},
 	rest: function(ev,obj){
 		//$(obj.el).css({ "-webkit-transform": "scale("+ 1 +")" });
 	}
 });
+
 
 $('.peppable2').pep({
 	debug: true, 
@@ -22,4 +28,3 @@ $('.peppable2').pep({
 		console.log( el.position().left, el.position().top, val )
 	}
 });
-
