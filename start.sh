6ce1f097ad7e2d978a90ce1d3f0e81bc82362766
chrisaiv <signup@chrisaiv.com>
2013-01-20 18:52:54 -0800

    updated start and stop

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
+ 
 # Invoke the Forever module (to START our Node.js server).
-~/magnifythetv/node_modules/forever/bin/forever \
+./node_modules/forever/bin/forever \
 start \
-#-al forever.log \
-#-ao out.log \
-#-ae err.log \
+-al forever.log \
+-ao out.log \
+-ae err.log \
 app.js
--- stop.sh
+++ stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+ 
 # Invoke the Forever module (to STOP our Node.js server).
 ./node_modules/forever/bin/forever stop app.js
