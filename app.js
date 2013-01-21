5d86b3f73b91193c5aa4d906e8bf70d1237c2bb8
chrisaiv <signup@chrisaiv.com>
2013-01-21 08:48:10 -0800

    updated favicon + titles

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 8080);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon( __dirname + '/public/favicon.ico', { maxAge: 2592000000 } ));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
